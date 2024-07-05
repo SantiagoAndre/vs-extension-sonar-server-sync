163760b518064ae4c735082b1fc271f747a9c5ba
Santiago Andres Ramirez Chavez <santiagoandres.ramirez@servinformacion.com>
2024-07-05 10:59:09 -0500

    commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,109 +1,118 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// Import necessary modules
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import  {SonarIssueController,SecurityHotspotController}  from './controller';
-import * as path from 'path';
+import { SonarIssueController, SecurityHotspotController } from './controller';
 
+// Define a class for the extension
+class SonarQubeExtension {
+    private issueLoader: SonarIssueController= {} as SonarIssueController ;
+    private securityLoader: SecurityHotspotController = {} as SecurityHotspotController;
+    private issuesDiagnostics: vscode.DiagnosticCollection = {} as vscode.DiagnosticCollection;
+    private hotspotsDiagnostics: vscode.DiagnosticCollection = {} as vscode.DiagnosticCollection;
 
+    constructor(private context: vscode.ExtensionContext) {}
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export  async function activate(context: vscode.ExtensionContext) {
-    const config = await getSonarConfig();
-    if (!config) {
-        vscode.window.showErrorMessage("SonarQube configuration not found in .vscode/sonar-config.json");
-        return;
-    }
-
-     // Crear colecciones de diagnósticos globales
-     const issuesDiagnostics = vscode.languages.createDiagnosticCollection('sonarIssues');
-     const hotspotsDiagnostics = vscode.languages.createDiagnosticCollection('sonarHotspots');
- 
- 
-     // Asegúrate de inicializar y configurar tus loaders aquí
-     const issueLoader = new SonarIssueController(config);
-     const securityLoader = new SecurityHotspotController(config);
- 
-     // Implementación de comandos con reutilización de colecciones globales
-     const loadIssuesCommand = vscode.commands.registerCommand('sonarext.loadSonarIssues', async () => {
-         const diagnostics = await issueLoader.fetchNextPage();
-         const count = updateDiagnostics(diagnostics, issuesDiagnostics);
-         vscode.window.showInformationMessage(`Fetched a page of SonarQube issues. Total: ${count}`);
-         context.subscriptions.push(issuesDiagnostics)
-     });
- 
-     const loadAllIssuesCommand = vscode.commands.registerCommand('sonarext.loadAllSonarIssues', async () => {
-         const diagnostics = await issueLoader.fetchAllPages();
-         const count = updateDiagnostics(diagnostics, issuesDiagnostics);
-         vscode.window.showInformationMessage(`All SonarQube issues loaded. Total issues loaded: ${count}`);
-         context.subscriptions.push(issuesDiagnostics)
-     });
- 
-     const loadSecurityHotspotsCommand = vscode.commands.registerCommand('sonarext.loadSecurityHostpots', async () => {
-         const diagnostics = await securityLoader.fetchNextPage();
-         const count = updateDiagnostics(diagnostics, hotspotsDiagnostics);
-         vscode.window.showInformationMessage(`Fetched a page of SonarQube security hotspots. Total: ${count}`);
-     });
- 
-     const loadAllSecurityHotspotsCommand = vscode.commands.registerCommand('sonarext.loadAllSecurityHostpots', async () => {
-         const diagnostics = await securityLoader.fetchAllPages();
-         const count = updateDiagnostics(diagnostics, hotspotsDiagnostics);
-         vscode.window.showInformationMessage(`All SonarQube security hotspots loaded. Total issues loaded: ${count}`);
-     });
- 
-     const clearIssuesCommand = vscode.commands.registerCommand('sonarext.clearIssues', () => {
-         issuesDiagnostics.clear();
-         vscode.window.showInformationMessage('All SonarQube issues have been cleared.');
-         issueLoader.restart()
-     });
- 
-     const clearHotspotsCommand = vscode.commands.registerCommand('sonarext.clearHotspots', () => {
-         hotspotsDiagnostics.clear();
-         vscode.window.showInformationMessage('All SonarQube Hotspots have been cleared.');
-         securityLoader.restart()
-     });
-    //  context.subscriptions.push(issuesDiagnostics, hotspotsDiagnostics);
- 
-     context.subscriptions.push(loadIssuesCommand, loadAllIssuesCommand, loadSecurityHotspotsCommand, loadAllSecurityHotspotsCommand, clearIssuesCommand, clearHotspotsCommand);
- }
- function updateDiagnostics(diagnosticsMap: Map<string, vscode.Diagnostic[]>, diagnosticsCollection: vscode.DiagnosticCollection): number {
-    let totalDiagnostics = 0;
-    diagnosticsMap.forEach((diagnostics, fileUri) => {
-        const uri = vscode.Uri.file(fileUri);
-
-        // Verificar y combinar diagnósticos existentes si es necesario
-        let existingDiagnostics = diagnosticsCollection.get(uri) || [];
-        let combinedDiagnostics = existingDiagnostics.concat(diagnostics);
-      
-        // Establecer los diagnósticos combinados en la colección
-        diagnosticsCollection.set(uri, combinedDiagnostics);
-
-        // Actualizar el contador total de diagnósticos
-        totalDiagnostics += diagnostics.length;
-    });
-    return totalDiagnostics;
-}
-
-
-async function getSonarConfig() {
-    try {
-        const files = await vscode.workspace.findFiles('.vscode/sonar-config.json', '**​/.vscode/**', 1);
-        if (files.length > 0) {
-            const content = fs.readFileSync(files[0].fsPath, 'utf8');
-            return JSON.parse(content);
+    // Method to activate the extension
+    async activate() {
+        const config = await this.getSonarConfig();
+        if (!config) {
+            vscode.window.showErrorMessage("SonarQube configuration not found in .vscode/sonar-config.json");
+            return;
         }
-        return null;
-    } catch (e) {
-        console.error("Error reading SonarQube configuration:", e);
-        return null;
+
+        // Create diagnostic collections
+        this.issuesDiagnostics = vscode.languages.createDiagnosticCollection('sonarIssues');
+        this.hotspotsDiagnostics = vscode.languages.createDiagnosticCollection('sonarHotspots');
+
+        // Initialize loaders
+        this.issueLoader = new SonarIssueController(config);
+        this.securityLoader = new SecurityHotspotController(config);
+
+        // Register commands
+        this.registerCommands();
     }
+
+    // Method to register VS Code commands
+    private registerCommands() {
+        this.context.subscriptions.push(
+            vscode.commands.registerCommand('sonarext.loadSonarIssues', async () => {
+                const diagnostics = await this.issueLoader.fetchNextPage();
+                const count = this.updateDiagnostics(diagnostics, this.issuesDiagnostics);
+                vscode.window.showInformationMessage(`Fetched a page of SonarQube issues. Total: ${count}`);
+            }),
+
+            vscode.commands.registerCommand('sonarext.loadAllSonarIssues', async () => {
+                const diagnostics = await this.issueLoader.fetchAllPages();
+                const count = this.updateDiagnostics(diagnostics, this.issuesDiagnostics);
+                vscode.window.showInformationMessage(`All SonarQube issues loaded. Total issues loaded: ${count}`);
+            }),
+
+            vscode.commands.registerCommand('sonarext.loadSecurityHostpots', async () => {
+                const diagnostics = await this.securityLoader.fetchNextPage();
+                const count = this.updateDiagnostics(diagnostics, this.hotspotsDiagnostics);
+                vscode.window.showInformationMessage(`Fetched a page of SonarQube security hotspots. Total: ${count}`);
+            }),
+
+            vscode.commands.registerCommand('sonarext.loadAllSecurityHostpots', async () => {
+                const diagnostics = await this.securityLoader.fetchAllPages();
+                const count = this.updateDiagnostics(diagnostics, this.hotspotsDiagnostics);
+                vscode.window.showInformationMessage(`All SonarQube security hotspots loaded. Total issues loaded: ${count}`);
+            }),
+
+            vscode.commands.registerCommand('sonarext.clearIssues', () => {
+                this.issuesDiagnostics.clear();
+                vscode.window.showInformationMessage('All SonarQube issues have been cleared.');
+                this.issueLoader.restart();
+            }),
+
+            vscode.commands.registerCommand('sonarext.clearHotspots', () => {
+                this.hotspotsDiagnostics.clear();
+                vscode.window.showInformationMessage('All SonarQube Hotspots have been cleared.');
+                this.securityLoader.restart();
+            })
+        );
+    }
+
+    // Helper method to update diagnostics in the collection
+    private updateDiagnostics(diagnosticsMap: Map<string, vscode.Diagnostic[]>, diagnosticsCollection: vscode.DiagnosticCollection): number {
+        let totalDiagnostics = 0;
+        diagnosticsMap.forEach((diagnostics, fileUri) => {
+            const uri = vscode.Uri.file(fileUri);
+            let existingDiagnostics = diagnosticsCollection.get(uri) || [];
+            let combinedDiagnostics = existingDiagnostics.concat(diagnostics);
+            diagnosticsCollection.set(uri, combinedDiagnostics);
+            totalDiagnostics += diagnostics.length;
+        });
+        return totalDiagnostics;
+    }
+
+    // Method to load SonarQube configuration from the .vscode folder
+    private async getSonarConfig(): Promise<any> {
+        try {
+            const files = await vscode.workspace.findFiles('.vscode/sonar-config.json', '**​/.vscode/**', 1);
+            if (files.length > 0) {
+                const content = fs.readFileSync(files[0].fsPath, 'utf8');
+                return JSON.parse(content);
+            }
+            return null;
+        } catch (e) {
+            console.error("Error reading SonarQube configuration:", e);
+            return null;
+        }
+    }
+
+    // Method to deactivate the extension
+    deactivate() {}
+}
+
+// Export a function to activate the extension
+export async  function activate(context: vscode.ExtensionContext) {
+    const extension = new SonarQubeExtension(context);
+    await extension.activate();
+}
+
+export function deactivate() {
 }
 
 
 
-
-
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
